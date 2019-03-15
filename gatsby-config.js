0123b156c0dec21db6d160a06ad89d907ad7abb8
Adam Hammes <ahammes@cortexmedia.ca>
2019-03-14 21:47:49 -0400

    Set display mode to fullscreen

--- gatsby-config.js
+++ gatsby-config.js
@@ -62,7 +62,7 @@ module.exports = {
         theme_color: `#663399`,
         display: `minimal-ui`,
         icon: `content/assets/gatsby-icon.png`,
-        display: `standalone`,
+        display: `fullscreen`,
       },
     },
     `gatsby-plugin-offline`,
