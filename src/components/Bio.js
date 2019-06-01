d585a22aecb1f8d0fef34027dee8b2eafc03126e
Adam Hammes <ahammes@cortexmedia.ca>
2019-06-01 13:22:00 -0400

    Add basic styles

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { StaticQuery, graphql } from 'gatsby'
 import Image from 'gatsby-image'
 
+import styles from './bio.module.scss'
 
 function Bio() {
   return (
@@ -10,8 +11,12 @@ function Bio() {
       render={data => {
         const { author } = data.site.siteMetadata
         return (
-          <div>
-            <Image fixed={data.avatar.childImageSharp.fixed} alt={author} />
+          <div class={styles.bioContainer}>
+            <Image
+              fixed={data.avatar.childImageSharp.fixed}
+              alt={author}
+              className={styles.profilePicture}
+            />
             <p>
               Written by <strong>{author}</strong>, a Yankee living and working
               in Québec.
--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -5,6 +5,8 @@ import Bio from '../components/Bio'
 import Layout from '../components/Layout'
 import SEO from '../components/seo'
 
+import styles from './post.module.scss'
+
 class BlogPostTemplate extends React.Component {
   render() {
     const post = this.props.data.markdownRemark
@@ -20,7 +22,7 @@ class BlogPostTemplate extends React.Component {
         <hr />
         <Bio />
 
-        <ul>
+        <ul className={styles.extraLinks}>
           <li>
             {previous && (
               <Link to={previous.fields.slug} rel="prev">
