bf496f475ab8ad55204407178e751d718aeab232
Adam Hammes <ahammes@cortexmedia.ca>
2019-06-26 23:01:31 -0500

    Add draft variable in frontmatter for hiding posts

--- src/components/listing.js
+++ src/components/listing.js
@@ -8,18 +8,20 @@ const Listing = ({ title, posts }) => (
   <Layout title={title} bioInHeader={true}>
     <main>
       <h1>{title}</h1>
-      {posts.map(({ node }) => {
-        const title = node.frontmatter.title || node.fields.slug;
-        return (
-          <article key={node.fields.slug}>
-            <h2>
-              <Link to={node.fields.slug}>{title}</Link>
-            </h2>
-            <DateTime dateString={node.frontmatter.date} />
-            <p>{node.frontmatter.description}</p>
-          </article>
-        );
-      })}
+      {posts
+        .filter(post => post.node.frontmatter.draft !== true)
+        .map(({ node }) => {
+          const title = node.frontmatter.title || node.fields.slug;
+          return (
+            <article key={node.fields.slug}>
+              <h2>
+                <Link to={node.fields.slug}>{title}</Link>
+              </h2>
+              <DateTime dateString={node.frontmatter.date} />
+              <p>{node.frontmatter.description}</p>
+            </article>
+          );
+        })}
     </main>
   </Layout>
 );
