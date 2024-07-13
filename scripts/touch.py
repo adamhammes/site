a233134b45ab91cdbb077cfd98ea8f1534984299
Adam Hammes <adam@hammes.io>
2024-07-13 12:55:14 -0500

    Fix deprecated method call in `touch.py`

--- scripts/touch.py
+++ scripts/touch.py
@@ -8,6 +8,6 @@ parser = argparse.ArgumentParser()
 parser.add_argument("file", type=pathlib.Path)
 args = parser.parse_args()
 
-date = datetime.datetime.utcnow().isoformat() + "Z"
+date = datetime.datetime.now(datetime.UTC).replace(microsecond=0).isoformat()
 with open(args.file, "w+") as f:
     f.write(f'+++\ndate = "{date}"\n+++\n\n')
